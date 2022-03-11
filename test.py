05e68de3b5fc11ed743ab7e08b713374862156e2
veles <velesiz00@gmail.com>
2022-03-11 22:43:30 +0300

    make it better

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 from datetime import datetime
 import telebot
-
+# fathomless-basin-37475
 token = "5185066102:AAE-Pxf7QOcDEi4APWGyOtCBfws9YSZNT68"
 def get_date():
     req = requests.get("https://yobit.net/api/3/ticker/usd_rur")
